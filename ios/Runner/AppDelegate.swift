9a5f22e03754c1b028560879310e41fbcaae7190
unknown <mahmoudlove1218@gmail.com>
2025-05-18 21:15:13 +0300

    Update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
-import Flutter
 import UIKit
+import Flutter
 import GoogleMaps
+import Firebase  // لازم تضيف Firebase هنا
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure()    // مهم جداً تفعّل Firebase
     GMSServices.provideAPIKey("AIzaSyCE8Z_S9GkPrz0_cvEHSF2j_UMIUiJgud8") 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
