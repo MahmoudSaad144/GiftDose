cd22baa424357b0d6f22d4732c46f422a3480d72
unknown <mahmoudlove1218@gmail.com>
2025-05-19 16:38:27 +0300

    Update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,26 +5,24 @@ import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  var window: UIWindow?  // أضفنا الـ window هنا
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 1. إعداد Firebase
     FirebaseApp.configure()
-    // 2. إعداد Google Maps API Key
     GMSServices.provideAPIKey("AIzaSyCE8Z_S9GkPrz0_cvEHSF2j_UMIUiJgud8")
-    
-    // 3. إنشاء الـ UIWindow وتثبيت FlutterViewController كـ root
-    window = UIWindow(frame: UIScreen.main.bounds)
-    let flutterVC = FlutterViewController()
-    // 4. تسجيل كل البلجنز مع الـ FlutterViewController
-    GeneratedPluginRegistrant.register(with: flutterVC)
-    window?.rootViewController = flutterVC
-    window?.makeKeyAndVisible()
 
-    // 5. استدعاء الـ super ويرجع true لو كل حاجة تمام
+    // إعداد نافذة التطبيق يدويًا
+    let flutterEngine = (self.window?.rootViewController as? FlutterViewController)?.engine ?? FlutterEngine(name: "my flutter engine")
+    flutterEngine.run()
+    
+    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = flutterViewController
+    self.window?.makeKeyAndVisible()
+
+    GeneratedPluginRegistrant.register(with: self)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
