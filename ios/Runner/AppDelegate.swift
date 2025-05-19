1ced3fc2fd9dc54b687a1e75b1a72f1e6e3e1d43
unknown <mahmoudlove1218@gmail.com>
2025-05-19 17:29:49 +0300

    Update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
-import Firebase
+import Firebase  // لازم تضيف Firebase هنا
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,20 +9,9 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-    GMSServices.provideAPIKey("AIzaSyCE8Z_S9GkPrz0_cvEHSF2j_UMIUiJgud8")
-
-    // إعداد نافذة التطبيق يدويًا
-    let flutterEngine = (self.window?.rootViewController as? FlutterViewController)?.engine ?? FlutterEngine(name: "my flutter engine")
-    flutterEngine.run()
-    
-    let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
-    self.window = UIWindow(frame: UIScreen.main.bounds)
-    self.window?.rootViewController = flutterViewController
-    self.window?.makeKeyAndVisible()
-
+    FirebaseApp.configure()    // مهم جداً تفعّل Firebase
+    GMSServices.provideAPIKey("AIzaSyCE8Z_S9GkPrz0_cvEHSF2j_UMIUiJgud8") 
     GeneratedPluginRegistrant.register(with: self)
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
