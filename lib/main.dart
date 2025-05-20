9915e583a3d1a595982e95c2210f7ff5c771fd59
unknown <mahmoudlove1218@gmail.com>
2025-05-20 13:09:56 +0300

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,190 @@
-// main.dart
-
-// عشان تتأكد إن مفيش بلاجن ولا صلاحيات بتتضايق:
-// أعمل كومنت لكل الـ imports اللي برا Flutter الأساسية
-// import 'package:giftdose/auth/forget%20password.dart';
-// import 'package:giftdose/auth/longin.dart';
-// import 'package:giftdose/auth/singup.dart';
-// import 'package:giftdose/navpar/darwar/occasions/add_occasions.dart';
-// import 'package:giftdose/navpar/darwar/occasions/occasions.dart';
-// import 'package:giftdose/navpar/darwar/profile/profile.dart';
-// import 'package:giftdose/navpar/darwar/settings.dart';
-// import 'package:giftdose/navpar/friends/userandfrinds.dart';
-// import 'package:giftdose/navpar/navbarpage.dart';
-// import 'package:giftdose/no_internet.dart';
-// import 'package:giftdose/startpage.dart';
-// import 'package:giftdose/translation/language_service.dart';
-// import 'package:giftdose/translation/translation_service.dart';
-// import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:giftdose/auth/forget%20password.dart';
+import 'package:giftdose/auth/longin.dart';
+import 'package:giftdose/auth/singup.dart';
+import 'package:giftdose/navpar/darwar/occasions/add_occasions.dart';
+import 'package:giftdose/navpar/darwar/occasions/occasions.dart';
+import 'package:giftdose/navpar/darwar/profile/profile.dart';
+import 'package:giftdose/navpar/darwar/settings.dart';
+import 'package:giftdose/navpar/friends/userandfrinds.dart';
+import 'package:giftdose/navpar/navbarpage.dart';
+import 'package:giftdose/no_internet.dart';
+import 'package:giftdose/startpage.dart';
+import 'package:giftdose/translation/language_service.dart';
+import 'package:giftdose/translation/translation_service.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:permission_handler/permission_handler.dart';
-// import 'package:get/get.dart';
-// import 'package:flutter_localizations/flutter_localizations.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(const MyApp());
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  bool hasInternet = await checkInternetConnection();
+  // await Firebase.initializeApp();
+  Get.put(ProfileController());
+  await setupNotifications();
+
+  Locale savedLocale = await LanguageService.getSavedLanguage();
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    runApp(MyApp(
+      savedLocale: savedLocale,
+      hasInternet: hasInternet,
+    ));
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<bool> checkInternetConnection() async {
+  var connectivityResult = await Connectivity().checkConnectivity();
+  return connectivityResult != ConnectivityResult.none;
+}
+
+class MyApp extends StatefulWidget {
+  final Locale savedLocale;
+  final bool hasInternet;
+  const MyApp(
+      {super.key, required this.savedLocale, required this.hasInternet});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Locale _locale;
+
+  @override
+  void initState() {
+    super.initState();
+    _locale = widget.savedLocale;
+    myrequestpermission();
+  }
+
+  void changeLocale(Locale locale) async {
+    await LanguageService.changeLanguage(locale);
+    setState(() {
+      _locale = locale;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'تجربة بسيطة',
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      translations: TranslationService(),
+      locale: _locale,
+      fallbackLocale: const Locale('en'),
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      initialRoute: widget.hasInternet ? "/" : "/no-internet",
+      getPages: [
+        GetPage(name: "/", page: () => StartPage()),
+        GetPage(name: "/no-internet", page: () => NoInternetPage()),
+        GetPage(name: "/1", page: () => Longinpage()),
+        GetPage(name: "/2", page: () => Singuppage()),
+        GetPage(name: "/3", page: () => Forgetpasswordpage()),
+        GetPage(name: "/4", page: () => Navbarpage()),
+        GetPage(name: "/5", page: () => ProfilePage()),
+        GetPage(name: "/6", page: () => AddOccasions()),
+        GetPage(name: "/7", page: () => Occasions()),
+        GetPage(name: "/8", page: () => SettingsPage()),
+        GetPage(name: "/10", page: () => UserAndFriendsPage()),
+      ],
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context)
+              .copyWith(textScaler: TextScaler.linear(1.0)),
+          child: child!,
+        );
+      },
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+Future<void> setupNotifications() async {
+  // FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('تجربة Codemagic'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // لمن تعمل زرار بسيط:
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('التطبيق شغال بدون بلاجين!')),
-            );
-          },
-          child: const Text('اضغط هنا'),
-        ),
-      ),
-    );
-  }
+  // إنشاء قناة الإشعارات
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.high,
+  );
+
+  // تهيئة الإشعارات المحلية
+  const AndroidInitializationSettings androidInitializationSettings =
+      AndroidInitializationSettings('@drawable/ic_notification');
+
+  final InitializationSettings initializationSettings =
+      InitializationSettings(android: androidInitializationSettings);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  // await messaging.requestPermission(
+  //   alert: true,
+  //   badge: true,
+  //   sound: true,
+  // );
+  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //   RemoteNotification? notification = message.notification;
+  //   if (notification != null) {
+  //     Get.find<ProfileController>().getData();
+  //   }
+  // });
 }
+
+myrequestpermission() async {
+  // FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // طلب إذن الإشعارات
+  // NotificationSettings settings = await messaging.requestPermission(
+  //   alert: true,
+  //   announcement: false,
+  //   badge: true,
+  //   carPlay: false,
+  //   criticalAlert: false,
+  //   provisional: false,
+  //   sound: true,
+  // );
+
+  // if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+  // } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+  //   print('User granted provisional notification permission');
+  // } else {
+  //   print('User declined or has not accepted notification permission');
+  // }
+
+  // طلب إذن الكاميرا
+  PermissionStatus cameraStatus = await Permission.camera.request();
+  if (cameraStatus.isGranted) {
+    print("Camera permission granted");
+  } else {
+    print("Camera permission denied");
+  }
+
+//   // تحديث الحالة في صفحة الإعدادات بعد طلب الأذونات
+  // updatePermissionStatus(cameraStatus, settings);
+}
+
+// void updatePermissionStatus(
+//     PermissionStatus cameraStatus, NotificationSettings settings) {}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import device_info_plus
+import file_selector_macos
+import flutter_local_notifications
+import flutter_localization
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
